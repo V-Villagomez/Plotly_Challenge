85481a23740dddabcbbdbfb81965e516b525a974
Vanessa Villagomez <villagomez.v@outlook.com>
2021-12-30 18:01:35 -0800

    initiate js file

---
+++ StarterCode/static/js/app.js
@@ -0,0 +1,15 @@
+// Function for change on dropdown menu
+function optionChanged(selectedID){
+
+    // verify that value is selected in dropdown
+    console.log(selectionID);
+
+    // Read sample json file for the data
+    d3.json("samples.json").then((data) => {
+       // console.log(data);
+});
+
+}
+
+// Initialize function
+//init();
