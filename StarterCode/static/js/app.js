5e71000c40722cd9b9abdad128b635f39c33c1f8
Vanessa Villagomez <villagomez.v@outlook.com>
2022-01-01 23:15:54 -0800

    update file

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,6 +1,25 @@
+// Function that would create a dynamic dropdown menu
+function optionChanged(selectedID){
+    // Verify if object is selected in dropdown
+    console.log(selectedID);
+}
+
 // Read sample json file for the data
-d3.json("samples.json").then((data) => {
+d3.json("samples.json").then ((data) => {
     console.log(data);
+
+// Clear dropdown as necessary
+d3.select("#selDataset").html("");
+
+// Select metadata array and for each item append item id and add ids to the dropdown
+data.metadata.forEach(item => {
+    d3.select ("#selDataset")
+      .append('option')
+      .attr('value', item.id)
+      .text(item.id);
+});
+
+
 
 })
 
